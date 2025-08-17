e1be8f5d6545d2c124f4e4f95cc2f837c5607dd6
Mena Erian <mena3koko2karas1@gmail.com>
2025-08-17 21:19:38 +0300

    Part 01 What is Delegate and Part 02 Delegate Example 01

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,67 @@
 ﻿namespace Demo
 {
+    /// Step 0. Delegate Declaration
+    public delegate int CustomFunc(string arg01);
+    /// NEW Delegate (Class), The Reference from this Delegate Can Refer to a Function or More [Pointer To Function]
+    /// These Functions Can be Class Member [Static] or Object Member [Non-Static].
+    /// These Functions Must Have the Same Signature of the Delegate: int (string).
+    /// Regardless Function Access Modifier, Regardless Naming (Function, Parameters).
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            /// Delegate is a C# Language Feature [C# 2.0]
+            /// Has 2 Usages:
+            /// 1. Functional Programming
+            /// 2. Event-Driven Programming
+
+            #region Delegate Example 01
+            /// CustomFunc reference;
+            /// // Step 1. Declare Delegate Reference
+            /// 
+            /// // Step 2. Initialize the Delegate Reference [Pointer To Function]
+            /// //reference = new CustomFunc(StringFunction.GetCountOfUpperCaseChars);
+            /// reference = StringFunction.GetCountOfUpperCaseChars; // "Syntax Sugar"
+            /// reference += StringFunction.GetCountOfLowerCaseChars; //  return for the last
+            /// 
+            /// reference -= StringFunction.GetCountOfLowerCaseChars;
+            /// 
+            /// //reference -= StringFunction.GetCountOfUpperCaseChars;
+            /// 
+            /// // Step 3. Use the Delegate Reference
+            /// //reference.Invoke("Mena Erian"); 
+            /// Console.WriteLine(reference("Mena Erian")); // "Syntax Sugar"
+            /// 
+            /// //int x = StringFunction.GetCountOfUpperCaseChars; 
+            #endregion
+        }
+    }
+    class StringFunction
+    {
+        //public StringFunction(int (string) Target)
+        //{
+
+        //}
+        public static int GetCountOfUpperCaseChars(string Name)
+        {
+            int count = 0;
+            Console.WriteLine("Upper");
+
+            if (Name is not null)
+                for (int i = 0; i < Name.Length; i++)
+                    if (char.IsUpper(Name[i])) count++;
+
+            return count;
+        }
+        public static int GetCountOfLowerCaseChars(string Name)
+        {
+            int count = 0;
+            Console.WriteLine("Lower");
+            if (Name is not null)
+                for (int i = 0; i < Name.Length; i++)
+                    if (char.IsLower(Name[i])) count++;
+
+            return count; 
         }
     }
 }
