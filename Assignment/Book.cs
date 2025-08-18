81f7134fb344a04998d0df27c244c7606dc5edb3
Mena Erian <mena3koko2karas1@gmail.com>
2025-08-18 17:05:05 +0300

    Assignment 03 : Delegate

---
+++ Assignment/Book.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+        public Book(string _ISBN, string _Title,
+        string[] _Authors, DateTime _PublicationDate,
+        decimal _Price)
+        {
+            ISBN = _ISBN;
+            Title = _Title;
+            Authors = _Authors;
+            PublicationDate = _PublicationDate;
+            Price = _Price;
+        }
+
+        public override string ToString()
+         => $"ISBN: {ISBN}, Title: {Title}, Authors: {Authors}, Publication: {PublicationDate:d}, Price: {Price:c}";
+    }
+    public class BookFunctions
+    {
+        public static string GetISBN(Book B) => B?.ISBN ?? "NOT FOUND";
+        public static string GetPublicationDate(Book B) 
+            => B?.PublicationDate.ToString() ?? "NOT FOUND";
+
+        public static string GetTitle(Book B) => B?.Title ?? "NOT FOUND";
+        public static string GetAuthors(Book B)
+        {
+            if (B == null) return "NOT FOUND";
+            string authors = "Authors: ";
+            int Count = 0;
+
+            foreach (string author in B.Authors)
+            {
+
+                if (Count < B.Authors.Length - 1) authors += $"{author}, ";
+                else authors += $"{author}";
+
+                Count++;
+            }
+
+            return authors;
+        }
+        public static string GetPrice(Book B) => B?.Price.ToString() ?? "NOT FOUND";
+    }
+}
